7ed59c1fb5bb2569f9916ef5c788b2c240301699
ryan Bowden <ryanbowden1@outlook.com>
2013-03-27 14:18:00 +0000

    Further work on the Registration system
    Added more files and re done Loads of part to include the universtiys in
    a list.

--- StaffRegister.php
+++ StaffRegister.php
@@ -70,3 +70,87 @@
         header('Location: StaffRegister.html');
     }
 ?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Staff registration &middot; QuizzIt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!--[if lt IE 9]>
+          <script src="assets/js/html5shiv.js"></script>
+        <![endif]-->
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <style type="text/css">
+            body {
+                padding-top: 40px;
+                padding-bottom: 40px;
+                background-color: #f5f5f5;
+            }
+
+            .form-register {
+                max-width: 400px;
+                padding: 19px 29px 29px;
+                margin: 0 auto 20px;
+                background-color: #fff;
+                border: 1px solid #e5e5e5;
+                -webkit-border-radius: 5px;
+                -moz-border-radius: 5px;
+                border-radius: 5px;
+                -webkit-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                -moz-box-shadow: 0 1px 2px rgba(0,0,0,.05);
+                box-shadow: 0 1px 2px rgba(0,0,0,.05);
+            }
+
+            .form-register .form-register-heading, .form-register .checkbox {
+                margin-bottom: 10px;
+            }
+
+            .form-register input[type="text"], .form-register input[type="password"] {
+                font-size: 16px;
+                height: auto;
+                margin-bottom: 15px;
+                padding: 7px 9px;
+            }
+        </style>
+        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+          <form method="POST" onsubmit="validateForm(this);" class="form-register">
+            <h2 class="form-register-heading">Staff registration</h2>
+            <p>Please fill in the fields below to create an account.</p>
+            <input type="text" class="input-block-level required" name="Tutor_ID" id="Tutor_ID" required="required" rel="popover" data-content="Please enter your ID" placeholder="Tutor ID">
+            <input type="text" class="input-block-level required" name="email" id="email" required="required" rel="popover" data-content="Please enter your email address" placeholder="Email">
+            <input type="text" class="input-block-level required" name="firstName" id="firstName" required="required" rel="popover" data-content="Please enter your first name" placeholder="First Name">
+            <input type="text" class="input-block-level required" name="lastName" id="lastName" required="required" rel="popover" data-content="Please enter your last name" placeholder="Last Name">
+            <input type="text" class="input-block-level required" name="university" id="university" required="required" rel"popover" data-content="Please enter your university" placeholder="University">
+            <input type="password" class="input-block-level required" name="password" id="password" required="required" rel="popover" data-content="Please enter a password" placeholder="Password">
+            <input type="password" class="input-block-level required" name="confirmPassword" id="confirmPassword" required="required" rel="popover" data-content="The passwords do not match" placeholder="Confirm Password">
+            <p>A validation email will be sent to the email address you provide. This is required to activate your account.</p>
+            <button class="btn btn-large btn-primary" name="submit" id="submit" type="submit">Register</button>
+            <a href="Default.html" style="margin-left: 5px;" class="btn btn-large">Back to HomePage</a>
+          </form>
+        </div>
+      <script src="http://code.jquery.com/jquery.js"></script>
+      <script src="assets/js/bootstrap.js"></script>
+      <script src="assets/js/jquery.js"></script>
+      <script src="assets/js/bootstrap-transition.js"></script>
+      <script src="assets/js/bootstrap-alert.js"></script>
+      <script src="assets/js/bootstrap-modal.js"></script>
+      <script src="assets/js/bootstrap-dropdown.js"></script>
+      <script src="assets/js/bootstrap-scrollspy.js"></script>
+      <script src="assets/js/bootstrap-tab.js"></script>
+      <script src="assets/js/bootstrap-tooltip.js"></script>
+      <script src="assets/js/bootstrap-popover.js"></script>
+      <script src="assets/js/bootstrap-button.js"></script>
+      <script src="assets/js/bootstrap-collapse.js"></script>
+      <script src="assets/js/bootstrap-carousel.js"></script>
+      <script src="assets/js/bootstrap-typeahead.js"></script>
+    </body>
+</html>
