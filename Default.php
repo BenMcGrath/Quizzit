cc333c812f947fd7ce00ad9019b51a12add9b112
Ben McGrath <mcgrathben1@gmail.com>
2013-03-29 00:01:09 +0000

    Changed more hyperlinks

--- Default.php
+++ Default.php
@@ -59,10 +59,10 @@ if (isset($_SESSION['username']))
           <div class="nav-collapse collapse">
             <ul class="nav">
               <li class="active"><a href="#">Profile</a></li>
-              <li><a href="About.html">About</a></li>
-              <li><a href="Contact.html">Contact</a></li>
-              <li><a href="tutorial.html">Tutorial</a></li>
-              <li><a href="AdminLogin.html">Admin</a></li>
+              <li><a href="About.php">About</a></li>
+              <li><a href="Contact.php">Contact</a></li>
+              <li><a href="tutorial.php">Tutorial</a></li>
+              <li><a href="AdminLogin.php">Admin</a></li>
 				<li class="dropdown">
 				<a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $username; ?><b class="caret"></b></a>
 				<ul class="dropdown-menu">
@@ -141,14 +141,14 @@ if (isset($_SESSION['username']))
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="brand" href="Default.html">QuizzIt</a>
+          <a class="brand" href="Default.php">QuizzIt</a>
           <div class="nav-collapse collapse">
             <ul class="nav">
-              <li class="active"><a href="Default.html">Home</a></li>
-              <li><a href="About.html">About</a></li>
-              <li><a href="Contact.html">Contact</a></li>
-              <li><a href="tutorial.html">Tutorial</a></li>
-              <li><a href="AdminLogin.html">Admin</a></li>
+              <li class="active"><a href="Default.php">Home</a></li>
+              <li><a href="About.php">About</a></li>
+              <li><a href="Contact.php">Contact</a></li>
+              <li><a href="tutorial.php">Tutorial</a></li>
+              <li><a href="AdminLogin.php">Admin</a></li>
             </ul>
           </div><!--/.nav-collapse -->
         </div>
