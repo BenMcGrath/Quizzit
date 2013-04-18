0840625714db38449a2088950d3dd3f2456197aa
ryan Bowden <ryanbowden1@outlook.com>
2013-04-18 09:35:01 +0100

    Work work continues on answer

--- Answer.php
+++ Answer.php
@@ -44,6 +44,39 @@
         //Now we have the quiz info let get a question
 
         //As this is the first question we need the first question
+        $questionID = $quizinfo['Q1_ID'];
+        //now we have the ID need to Check there a code in there.
+        if($questionID == "")
+        {
+            //No questions?
+            //Bye Bye
+            header('Location: StudentcodeGUI.php');
+
+        }
+        else
+        {
+            //Need to get the Question Information
+            $Questquery = "SELECT * FROM Questions Where ID = '$questionID'";
+            $Questarray = mysql_query($Questquery);
+                    
+            //$quesinfo = mysql_fetch_array($QuestInfo);
+            //
+            if($Questarray)
+            {
+
+                $row = mysql_fetch_array($Questarray);
+
+                        
+            }
+            else
+            {
+                //It failed
+                echo $questionID;
+
+            }
+
+                        
+        }
 
 
         //Run page
@@ -73,83 +106,77 @@
 
 ?>
 <!DOCTYPE html>
-	<html>
+<html>
 	<head>
-	<meta charset="utf-8">
-    <title>Home &middot; QuizzIt</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+	    <meta charset="utf-8">
+        <title>Home &middot; QuizzIt</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
     
-    <!--[if lt IE 9]>
-      <script src="assets/js/html5shiv.js"></script>
-    <![endif]-->
+        <!--[if lt IE 9]>
+          <script src="assets/js/html5shiv.js"></script>
+        <![endif]-->
 
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <style>
-        body {
-            padding-top: 60px;
-        }
-    </style>
-    <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-  </head>
+        <link href="assets/css/bootstrap.css" rel="stylesheet">
+        <style>
+            body {
+                padding-top: 60px;
+            }
+        </style>
+        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
+    </head>
 
-  <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="navbar-inner">
-        <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="brand" href="Default.php">QuizzIt</a>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-              <li class="active"><a href="Default.php">Home</a></li>
-              <li><a href="About.php">About</a></li>
-              <li><a href="Contact.php">Contact</a></li>
-              <li><a href="Tutorial.php">Tutorial</a></li>
-              <li><a href="AdminLogin.php">Admin</a></li>
-            </ul>
-          </div><!--/.nav-collapse -->
+    <body>
+        <div class="navbar navbar-inverse navbar-fixed-top">
+          <div class="navbar-inner">
+            <div class="container">
+              <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="brand" href="Default.php">QuizzIt</a>
+              <div class="nav-collapse collapse">
+                <ul class="nav">
+                  <li class="active"><a href="Default.php">Home</a></li>
+                  <li><a href="About.php">About</a></li>
+                  <li><a href="Contact.php">Contact</a></li>
+                  <li><a href="Tutorial.php">Tutorial</a></li>
+                  <li><a href="AdminLogin.php">Admin</a></li>
+                </ul>
+              </div><!--/.nav-collapse -->
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
-    <div class="container">
-      <div class="hero-unit">
-        <h1>Welcome to QuizzIt</h1>
-        <br>
-        <h2>Who are you?</h2>
-        <p>Please select the button relevant to you below, staff or student.</p>
-        <a href="LoginGUI.php" class="btn btn-large btn-primary">Staff</a>
-        <a href="StudentcodeGUI.php" style="margin-left: 5px;" class="btn btn-large">Student</a>
-        <br><br>
-        <h2>Staff registration</h2>
-        <p>If you are a 'first-time visiting' staff member, you will need to create an account on this system.</p>
-        <a href="TutorRegister.php" class="btn btn-large btn-primary">Staff Registration</a>
-      </div>
-    </div>
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-    <script>
-        $('.dropdown-toggle').dropdown()
-    </script>
+        <div class="container">
+          <div class="hero-unit">
+              <?php
+                  
+              ?>
+
+          </div>
+        </div>
+        <script src="assets/js/jquery.js"></script>
+        <script src="assets/js/bootstrap-transition.js"></script>
+        <script src="assets/js/bootstrap-alert.js"></script>
+        <script src="assets/js/bootstrap-modal.js"></script>
+        <script src="assets/js/bootstrap-dropdown.js"></script>
+        <script src="assets/js/bootstrap-scrollspy.js"></script>
+        <script src="assets/js/bootstrap-tab.js"></script>
+        <script src="assets/js/bootstrap-tooltip.js"></script>
+        <script src="assets/js/bootstrap-popover.js"></script>
+        <script src="assets/js/bootstrap-button.js"></script>
+        <script src="assets/js/bootstrap-collapse.js"></script>
+        <script src="assets/js/bootstrap-carousel.js"></script>
+        <script src="assets/js/bootstrap-typeahead.js"></script>
+        <script>
+            $('.dropdown-toggle').dropdown()
+        </script>
 	</body>
-	</html>
+</html>
 
 
 
